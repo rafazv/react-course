569eb36d1741e0c8c667bf672ec857fce6721ff4
Rafaela <rafaela.lima@indt.org.br>
2020-03-06 10:53:56 -0400

    First Assingment

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+export class DishDetail extends Component {
+
+    renderDish(dish) {
+        if(dish != null) {
+            return(
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg top src={dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <h4>Comments</h4>
+                        {this.renderComments(dish.comments)}
+                    </div>
+                </div>
+            );
+        } else {
+            return(
+                <div></div>
+            );
+        }
+    }
+
+    renderComments(comments) {
+        if(comments != null) {
+            const comment = comments.map(obj => {
+                return (
+                    <div key={obj.id}>
+                        <p>{obj.comment}</p>
+                        <p>-- {obj.author}, {obj.date}</p>
+                    </div>
+                );
+            });
+            return comment;
+        } else {
+            return(
+                <div></div>
+            );
+        }
+    }
+
+
+    render() {
+        return this.renderDish(this.props.dish);
+    }
+}
+
+export default DishDetail;
