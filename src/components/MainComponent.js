8e374d037140b90c8e016db8ad25ebc7e26367d6
Rafaela <rafaela.lima@indt.org.br>
2020-03-12 13:32:37 -0400

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import {Switch, Route, Redirect} from 'react-router-dom';
 import Contact from './ContactComponent';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
+import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 
@@ -34,11 +35,22 @@ class Main extends Component {
                 />
             );
         }
+
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail
+                    dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                />
+            );
+        };
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
